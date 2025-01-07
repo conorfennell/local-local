b3af3a344d06efb04c130fe7d7ac2e19575e76e5
conorfennell <fennell.conor@gmail.com>
2025-01-07 21:54:38 +0000

    chore: Update docker-compose command to use 'docker compose' syntax

--- start_docker.sh
+++ start_docker.sh
@@ -4,4 +4,4 @@
 COMMIT_ID=$(git rev-parse --short HEAD)
 
 # Build and start the Docker Compose services with the commit ID as an environment variable
-docker-compose up --build -d --env-file <(echo "COMMIT_ID=$COMMIT_ID")
+docker compose up --build -d --env-file <(echo "COMMIT_ID=$COMMIT_ID")
